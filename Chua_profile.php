cb484c5698719712f6b541bd567b180d9b4a605f
Amber-0666 <limxien2006@gmail.com>
2025-05-02 10:58:34 +0800

    Update php, navbar and footer

--- Chua_profile.php
+++ Chua_profile.php
@@ -9,45 +9,7 @@
 </head>
 <body class="profile-body">
 
-    <nav class="navbar">
-        <div class="nav-logo">
-            <a href="index.html"><img src="styles/images/logo.png" alt="Brew N Go Logo"></a>
-        </div>
-        
-        <input type="checkbox" id="nav-toggle" class="nav-toggle">
-            <label for="nav-toggle" class="nav-toggle-label">
-                <span></span>
-                <span></span>
-                <span></span>
-            </label>
-
-        <ul class="nav-menu">
-            <li class="nav-dropdown">
-                <a href="#" class="nav-dropbtn">PRODUCTS</a>
-                <div class="nav-dropdown-content">
-                    <a href="product1.html">Basic Brew</a>
-                    <a href="product2.html">Artisan Brew</a>
-                    <a href="product3.html">Non-Coffee</a>
-                    <a href="product4.html">Hot Beverages</a>
-                </div>
-            </li>
-
-            <li class="nav-dropdown">
-                <a href="#" class="nav-dropbtn">ACTIVITIES</a>
-                <div class="nav-dropdown-content">
-                    <a href="coming_soon.html">Coming Soon</a>
-                    <a href="current.html">Current Activities</a>
-                    <a href="past_activities.html">Past Activities</a>
-                </div>
-            </li>
-
-            <li ><a href="joinus.html">JOIN US</a></li>                
-            <li><a href="enquiry.html">ENQUIRY FORM</a></li>
-            <li><a href="registration.html">MEMBERSHIP REGISTRATION</a></li>
-            <li><a href="login.html">LOGIN</a></li>  
-       </ul>
-    </nav>
-
+    <?php include 'navbar.php'; ?>
 
     <div class="profile-container">
         <img src="styles/images/Chua_profile.png" alt="Chua Yong Kang" class="profile-pic">
---
+++ Roxas_profile.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Roxas's Profile</title>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+
+<body class="profile-body">
+
+    <?php include 'navbar.php'; ?>
+
+    <div class="profile-container">
+        <img src="styles/images/Roxas_profile.jpeg" alt="Roxas Xuan Hao TEO" class="profile-pic">
+        <h1 class="profile-h1">Roxas Teo Xuan Hao (104405272)
+            <br> Course: Bachelor of Computer Science
+        </h1>
+        
+        <table class="profile-table">
+            <tr>
+                <th class="profile-th">Demographic Info</th>
+                <td class="profile-td">I'm Roxas Teo, a 19 year old computer science students who recently started my first year in Swinburne. I am quite introverted and oftern like to spend my free time alone reading and playing games instead of socializing. However, if push comes to shove, I can still muster out courage to interact with others.</td>
+            </tr>
+            <tr>
+                <th class="profile-th">Description of Hometown</th>
+                <td class="profile-td">I come from Kota Kinabalu, a capital in Sabah state of Malaysia. We're famous for our delicacies such as our Sabah Laksa and popular spots such as our night bazaar. Here in Sabah, you also get to experience some of our beautiful nature scenery outside from the city. </td>
+            </tr>
+            <tr>
+                <th class="profile-th">Great Achievement</th>
+                <td class="profile-td">Although I did not partake in many competitons, I had participate in many volunteer work back in high school, a few examples would be social services and help out with any major events back in high school. Back in my club, I am also proud of helping out those who are in need such as the elederly home or partake in supporting the underpriviledged children in Sabah.</td>
+            </tr>
+            <tr>
+                <th  class="profile-th">Favourite Music</th> 
+                <td class="profile-td">
+ 			<ul>
+				<li><a href="https://youtu.be/P7drJS1vhl4?si=0DyHeP3IyP4YMm3_" target="_blank">陈雪凝 - 绿色</li>
+				<li><a href="https://youtu.be/CkvWJNt77mU?si=lVgWU7GLBv76e27D" target="_blank">Yorushika - Sunny</li>
+				<li><a href=""https://youtu.be/ony539T074w?si=dzGNis1zFeI30kQk target="_blank">King Gnu - Hakujitsu</li>
+			</ul>
+                </td>
+            </tr>
+        </table>
+
+        <div class="profile-button">
+            <a href="mailto:104405272@students.swinburne.edu.my">Contact me</a>
+        </div>
+
+    </div>
+
+    <?php include 'footer.php'; ?>
+
+</body>       
+</html>
