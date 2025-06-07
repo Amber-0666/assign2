1b54d4588accfb4c8d4df00347e7d57e3b6a5e60
andersonchua1125 <andersonchuayk02@gmail.com>
2025-06-07 15:50:38 +0800

    update on database

--- enquiry_process.php
+++ enquiry_process.php
@@ -7,7 +7,7 @@ ini_set('display_errors', 1);
 $host = 'localhost';
 $username = 'root';
 $password = '';
-$database = 'enquiry';
+$database = 'brewngo';
 $conn = new mysqli($host, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
