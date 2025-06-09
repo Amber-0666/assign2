208b89199d1a692608795ec9392df3e0c1d5b7e5
Amber-0666 <limxien2006@gmail.com>
2025-06-09 22:13:21 +0800

    Update

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -23,7 +23,8 @@
         <h2>Welcome back, Admin!</h2>
         <p id="admin-msg">
             This is your dashboard, where you can manage all user accounts efficiently. <br>
-            From here, you can <strong>add</strong>, <strong>edit</strong>, or <strong>delete</strong> user information, as well as monitor system activity and maintain overall site integrity.
+            From here, you can <strong>add</strong>, <strong>edit</strong>, or <strong>delete</strong> user information, 
+            as well as monitor system activity and maintain overall site integrity.
         </p>
         <p id="admin-msg">
             Use the navigation menu beside to access the user management panel, update user information, or view reports.
